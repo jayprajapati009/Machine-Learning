130984dbd2b68a97380e085e1581ae9175ee61ba
jayprajapati009 <jayprajapati9900@gmail.com>
2022-01-20 19:27:03 +0530

    Taking Care of Missing Data Block Updated

--- Data Preprossing/data_preprocessing_tools.py
+++ Data Preprossing/data_preprocessing_tools.py
@@ -4,25 +4,35 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.impute import SimpleImputer as si
 
 # Importing the Data set
 
-# This Line Gives Error
-# dataSet = pd.read_csv("C:\Users\jaypr\Desktop\VSCodes\Machine Learning\Udemy Course\My Prac\Data Preprossing\Data.csv")
+r"""
+This Line Gives Error
+dataSet = pd.read_csv("C:\Users\jaypr\Desktop\VSCodes\Machine Learning\Udemy Course\My Prac\Data Preprossing\Data.csv")
 
-# Follwing two lines won't give error
+Follwing two lines won't give error
 
-# (1) here 'r' converts the string into raw string
-# dataSet = pd.read_csv(r"C:\Users\jaypr\Desktop\VSCodes\Machine Learning\Udemy Course\My Prac\Data Preprossing\Data.csv")
-# (2) here the back slash (/) workd to solve the error 
-# dataSet = pd.read_csv("C:/Users/jaypr/Desktop/VSCodes/Machine Learning/Udemy Course/My Prac/Data Preprossing/Data.csv")
+(1) here 'r' converts the string into raw string
+dataSet = pd.read_csv(r"C:\Users\jaypr\Desktop\VSCodes\Machine Learning\Udemy Course\My Prac\Data Preprossing\Data.csv")
+(2) here the back slash (/) workd to solve the error 
+dataSet = pd.read_csv("C:/Users/jaypr/Desktop/VSCodes/Machine Learning/Udemy Course/My Prac/Data Preprossing/Data.csv") 
+"""
 
-
-dataSet = pd.read_csv("C:/Users/jaypr/Desktop/VSCodes/Machine Learning/Udemy Course/My Prac/Data Preprossing/Data.csv")
+dataSet = pd.read_csv(
+    r"C:\Users\jaypr\Desktop\VSCodes\Machine Learning\Udemy Course\Machine-Learning\Data Preprossing\Data.csv")
 
 x = dataSet.iloc[:, :-1].values
 y = dataSet.iloc[:, -1].values
 
+# Taking Care of Missing Data
+
+imputer = si(missing_values=np.nan, strategy='mean')
+imputer.fit(x[:, 1:3])
+x[:, 1:3] = imputer.transform(x[:, 1:3])
+
 print(x)
 print(y)
+
 
