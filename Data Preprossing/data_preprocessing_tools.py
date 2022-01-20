508f339894235343630bc1dcd0490d038cba1ad3
jayprajapati009 <jayprajapati9900@gmail.com>
2022-01-20 19:45:47 +0530

    One Hot Encoding Done

--- Data Preprossing/data_preprocessing_tools.py
+++ Data Preprossing/data_preprocessing_tools.py
@@ -1,10 +1,14 @@
 # Data Preprocessing Tools
 
 # Importing Library
+import imp
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.impute import SimpleImputer as si
+from sklearn.impute import SimpleImputer
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+import transformers
 
 # Importing the Data set
 
@@ -16,7 +20,7 @@ Follwing two lines won't give error
 
 (1) here 'r' converts the string into raw string
 dataSet = pd.read_csv(r"C:\Users\jaypr\Desktop\VSCodes\Machine Learning\Udemy Course\My Prac\Data Preprossing\Data.csv")
-(2) here the back slash (/) workd to solve the error 
+(2) here the back slash (/) worked to solve the error 
 dataSet = pd.read_csv("C:/Users/jaypr/Desktop/VSCodes/Machine Learning/Udemy Course/My Prac/Data Preprossing/Data.csv") 
 """
 
@@ -28,11 +32,15 @@ y = dataSet.iloc[:, -1].values
 
 # Taking Care of Missing Data
 
-imputer = si(missing_values=np.nan, strategy='mean')
+imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
 imputer.fit(x[:, 1:3])
 x[:, 1:3] = imputer.transform(x[:, 1:3])
 
+# Encoding Categorical Data
+
+_ct = ColumnTransformer(
+    transformers=[('encoder', OneHotEncoder(), [0])], remainder='passthrough')
+x = np.array(_ct.fit_transform(x))
+
 print(x)
 print(y)
-
-
