7cff8c0be14a1372c5ab0d31140b44d912caedce
jayprajapati009 <jayprajapati9900@gmail.com>
2022-01-21 09:40:17 +0530

    Feature Scaling Done

--- Data Preprossing/data_preprocessing_tools.py
+++ Data Preprossing/data_preprocessing_tools.py
@@ -6,21 +6,21 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.impute import SimpleImputer
 from sklearn.compose import ColumnTransformer
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder, StandardScaler
 from sklearn.model_selection import train_test_split
 
 # Importing the Data set
 
 r"""
 This Line Gives Error
-dataSet = pd.read_csv("C:\Users\jaypr\Desktop\VSCodes\Machine Learning\Udemy Course\My Prac\Data Preprossing\Data.csv")
+dataSet = pd.read_csv("C:\Users\jaypr\Desktop\VSCodes\Machine Learning\Udemy Course\Machine-Learning\Data Preprossing\Data.csv")
 
 Follwing two lines won't give error
 
 (1) here 'r' converts the string into raw string
-dataSet = pd.read_csv(r"C:\Users\jaypr\Desktop\VSCodes\Machine Learning\Udemy Course\My Prac\Data Preprossing\Data.csv")
+dataSet = pd.read_csv(r"C:\Users\jaypr\Desktop\VSCodes\Machine Learning\Udemy Course\Machine-Learning\Data Preprossing\Data.csv")
 (2) here the back slash (/) worked to solve the error 
-dataSet = pd.read_csv("C:/Users/jaypr/Desktop/VSCodes/Machine Learning/Udemy Course/My Prac/Data Preprossing/Data.csv") 
+dataSet = pd.read_csv("C:/Users/jaypr/Desktop/VSCodes/Machine Learning/Udemy Course/Machine-Learning/Data Preprossing/Data.csv") 
 """
 
 dataSet = pd.read_csv(
@@ -50,6 +50,12 @@ y = _le.fit_transform(y)
 
 x_train, x_test, y_train, y_test = train_test_split(
     x, y, test_size=0.2, random_state=1)
+
+# Feature Scaling
+
+_sc = StandardScaler()
+x_train[:, 3:] = _sc.fit_transform(x_train[:, 3:])
+x_test[:, 3:] = _sc.transform(x_test[:, 3:])
 
 print(x_train)
 print(x_test)
