3d2050cbd1cc1f872a1274be70e6e8b92252c6d0
jayprajapati009 <jayprajapati9900@gmail.com>
2022-01-25 13:36:34 +0530

    MLR update

--- (2) Regression/(2.2) Multiple Linear Regression/Multiple_Linear_Regression.py
+++ (2) Regression/(2.2) Multiple Linear Regression/Multiple_Linear_Regression.py
@@ -1,6 +1,7 @@
 # Data Preprocessing Template
 
 # Importing the libraries
+from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 import numpy as np
 import matplotlib.pyplot as plt
@@ -24,7 +25,11 @@ x = np.array(_ct.fit_transform(x))
 x_train, x_test, y_train, y_test = train_test_split(
     x, y, test_size=0.2, random_state=0)
 
-print(x_train)
-print(x_test)
-print(y_train)
-print(y_test)
+# Training the Multiple Linear Regression model on the Training set
+_regressor = LinearRegression()
+_regressor.fit(x_train, y_train)
+
+# Predicting the Test set results
+y_pred = _regressor.predict(x_test)
+np.set_printoptions(precision=2)
+print(np.concatenate((y_pred.reshape(len(y_pred), 1), y_test.reshape(len(y_test),1)),1))
